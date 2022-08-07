d9b5de362d95e39468b0edef491651438666dfae
Renata Karolina <renatakarolinarko@gmail.com>
2022-08-07 10:05:23 -0400

    add api

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Home } from "./page/home";
 
+import "./style/global.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Home />
